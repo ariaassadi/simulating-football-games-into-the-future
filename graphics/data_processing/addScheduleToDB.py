2a3a8d0c4c1a4c1bd38d564a825035b55ff4ad5b
Oskar Rick <oskar.rick@gmail.com>
2024-03-11 12:33:41 +0100

    added script to fill the database

--- graphics/data_processing/addScheduleToDB.py
+++ graphics/data_processing/addScheduleToDB.py
@@ -13,22 +13,38 @@ data_path = path + '/data_processing' + '/data' + '/games'
 match_paths = glob.glob(os.path.join(data_path, "*.parquet"))
 match_ids = [os.path.splitext(os.path.basename(path))[0] for path in match_paths]
 
-frames_dfs = []
+# Dictionary to convert long name to short name:
+df_club_info = pd.read_excel(path + '/data_processing' + '/data' + '/club_info.xlsx')
 
-for match_id in match_ids:
+long_to_short_name = {item['Team']: item['Short Name'] for item in df_club_info[["Team", "Short Name"]].to_dict(orient='records')}
+
+print(long_to_short_name['BK Häcken'])
+path_to_db = path + '/data_processing' + '/data' + '/2sec.sqlite'
+
+
+schedule_df = pd.DataFrame(columns=['match_id', 'home_team_name', 'away_team_name', 'home_team_name_short', 'away_team_name_short'])
+write_to_db = False
+
+for i, match_id in enumerate(match_ids):
     file_path_match = f"{data_path}/{match_id}.parquet"
     print(file_path_match)
     frames_df = pd.read_parquet(file_path_match)
-    frames_dfs.append(frames_df)
-    
-for i in range(len(frames_dfs)):
-    frames_dfs[i].drop(columns=['minute', 'second', 'ball_in_motion', 'a_x', 'a_y', 'role', 'distance_ran', 'events'], inplace=True)
+    frames_df.drop(columns=['minute', 'second', 'ball_in_motion', 'a_x', 'a_y', 'role', 'distance_ran', 'events'], inplace=True)
+    if write_to_db:
+        # Connect to the SQLite database
+        conn = sqlite3.connect(path_to_db)
 
-schedule_df = []
+        # Iterate over each DataFrame in frames_dfs and write it to the database
 
+        if (i==0):
+            table_name = f'games'
+            frames_df.to_sql(table_name, conn, if_exists='replace')
+        else:
+            table_name = f'games'
+            frames_df.to_sql(table_name, conn, if_exists='append')
 
-
-for i, frames_df in enumerate(frames_dfs):
+        # Close the connection
+        conn.close()
     frames_df['team_name'] = frames_df['team_name'].astype(str)    
     
     home_team_df = frames_df[frames_df['team'] == 'home_team']
@@ -38,24 +54,22 @@ for i, frames_df in enumerate(frames_dfs):
     home_team_name = home_team_df['team_name'].iloc[0]
     away_team_name = away_team_df['team_name'].iloc[0]
     
-    schedule_df.append(pd.DataFrame({'match_id': match_id, 'home_team_name': home_team_name, 'away_team_name': away_team_name}, index=[i]))
-print(schedule_df[0])
+    new_df = pd.DataFrame({'match_id': match_id, 'home_team_name': home_team_name, 'away_team_name': away_team_name, 'home_team_name_short': long_to_short_name[home_team_name], 'away_team_name_short': long_to_short_name[away_team_name]}, index=[i])
+    
+    # concat the new_df to the schedule_df
+    schedule_df = pd.concat([schedule_df, new_df])
+    
+print(schedule_df)
 
-write_to_db = False
+write_to_db = True
 
-if write_to_db:
-    path_to_db = path + '/data_processing' + '/data' + '/2sec.sqlite'
+if True:
+    print('Writing to DB' + path_to_db)
     # Connect to the SQLite database
     conn = sqlite3.connect(path_to_db)
 
-    # Iterate over each DataFrame in frames_dfs and write it to the database
-    for i, df in enumerate(frames_dfs):
-        if (i==0):
-            table_name = f'games'
-            df.to_sql(table_name, conn, if_exists='replace')
-        else:
-            table_name = f'games'
-            df.to_sql(table_name, conn, if_exists='append')
+    table_name = f'schedule'
+    schedule_df.to_sql(table_name, conn, if_exists='replace')
 
     # Close the connection
     conn.close()
