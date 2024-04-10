edf787fbf57c0fc2323c50b0d896c965983cf8ed
Oskar Rick <oskar.rick@gmail.com>
2024-04-10 20:40:26 +0200

    refactored CalculateDistance, ShowPlayerNames, and MovePlayer

--- graphics/unity/Assets/Scripts/Tools/Tool.cs
+++ graphics/unity/Assets/Scripts/Tools/Tool.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using Eggs;
+
 public abstract class Tool : MonoBehaviour
 {
-    // private GameObject[] players;
+    protected GameObject[] players;
     protected Image border;
+    protected ToolManager toolManager;
 
-    private void Start()
+    public bool uniqueTool = false;
+
+    protected virtual void Start()
     {
         border = transform.Find("Border").GetComponent<Image>();
+        toolManager = GameObject.Find("ToolManager").GetComponent<ToolManager>();
         if (border == null)
         {
             Debug.LogError("Border not found");
@@ -20,9 +26,89 @@ public abstract class Tool : MonoBehaviour
         {
             Debug.Log("Border found for " + this.name);
         }
+        if (toolManager == null)
+        {
+            Debug.LogError("ToolManager not found");
+            return;
+        }
+        else
+        {
+            Debug.Log("ToolManager found for " + this.name);
+        }
     }
 
-    public abstract void Select();
-    public abstract void Deselect();
-    public abstract void Update();
+    protected GameObject SelectPlayer()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.tag == "Player" || hit.collider.gameObject.tag == "Ball")
+                {
+                    return hit.collider.gameObject;
+                }
+            }
+        }
+        return null;
+    }
+
+    protected void ResetPlayers(int nrPlayers)
+    {
+        if (players != null)
+        {
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (players[i] != null)
+                {
+                    DehighlightPlayer(players[i]);
+                    Debug.Log("Player deselected: " + players[i].name);
+                }
+            }
+        }
+
+        players = new GameObject[nrPlayers];
+    }
+
+    protected void HighlightPlayer(GameObject player)
+    {
+        player.transform.GetChild(0).gameObject.SetActive(true);
+        if (player.tag == "Player")
+        {
+            ShowPlayerName(player);
+        }
+        Debug.Log("Player selected: " + player.name);
+    }
+
+    protected void DehighlightPlayer(GameObject player)
+    {
+        player.transform.GetChild(0).gameObject.SetActive(false);
+        if (player.tag == "Player")
+        {
+            HidePlayerName(player);
+        }
+    }
+
+    protected void ShowPlayerName(GameObject player)
+    {
+        player.transform.Find("Canvas").GetComponent<EggUI>().SetTextPlayerName();
+    }
+
+    protected void HidePlayerName(GameObject player)
+    {
+        player.transform.Find("Canvas").GetComponent<EggUI>().SetEmptyText();
+    }
+
+    public virtual void Select()
+    {
+        border.color = Color.white;
+    }
+
+    public virtual void Deselect()
+    {
+        border.color = Utils.HexToColor("#12326e");
+    }
+
+    public abstract void UpdateTool();
 }
--- graphics/unity/Assets/Scripts/Tools/ToolManager.cs
+++ graphics/unity/Assets/Scripts/Tools/ToolManager.cs
@@ -5,60 +5,140 @@ using UnityEngine.UI;
 
 public class ToolManager : MonoBehaviour
 {
-    private Tool currentTool;
+    private Tool[] activeTools;
+    private GameObject[] players;
+
+    public void Update()
+    {
+        if (activeTools != null)
+        {
+            foreach (Tool tool in activeTools)
+            {
+                tool.UpdateTool();
+            }
+        }
+    }
 
     public void SelectTool(GameObject toolObject)
     {
         Tool tool = toolObject.GetComponent<Tool>();
         if (tool != null)
         {
-            if (currentTool != null)
+            if (tool.uniqueTool)
             {
-                currentTool.Deselect();
+                if (ToolInActiveTools(tool))
+                {
+                    DeselectTool(tool);
+                }
+                else
+                {
+                    DeselectTools();
+                    SelectToolHelper(tool);
+                }
             }
-            if (currentTool == tool)
+            else if (ToolInActiveTools(tool))
             {
-                currentTool.Deselect();
-                currentTool = null;
+                DeselectTool(tool);
             }
             else
             {
-                currentTool = tool;
-                currentTool.Select();
+                SelectToolHelper(tool);
+                if (players == null)
+                    Debug.Log("Number of players in manager select: " + 0);
+                else
+                    Debug.Log("Number of players in manager select: " + players.Length);
+
             }
         }
     }
 
-    // public void Update()
-    // {
-    //     if (currentTool != null)
-    //         currentTool.Update();
-    // }
+    private void AddToSelectedTools(Tool tool)
+    {
+        if (activeTools == null)
+        {
+            activeTools = new Tool[1];
+            activeTools[0] = tool;
+        }
+        else
+        {
+            Tool[] newActiveTools = new Tool[activeTools.Length + 1];
+            for (int i = 0; i < activeTools.Length; i++)
+            {
+                newActiveTools[i] = activeTools[i];
+            }
+            newActiveTools[activeTools.Length] = tool;
+            activeTools = newActiveTools;
+        }
+    }
+
+    private bool ToolInActiveTools(Tool tool)
+    {
+        if (activeTools != null)
+        {
+            foreach (Tool activeTool in activeTools)
+            {
+                if (activeTool == tool)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // If any tool that has property uniqueToo set to true, deselect that tool
+    private void SelectToolHelper(Tool tool)
+    {
+        if (activeTools != null)
+        {
+            foreach (Tool activeTool in activeTools)
+            {
+                if (activeTool.uniqueTool)
+                {
+                    DeselectTool(activeTool);
+                }
+            }
+        }
+        tool.Select();
+        AddToSelectedTools(tool);
+    }
+
+    private void DeselectTool(Tool tool)
+    {
+        if (activeTools != null)
+        {
+            Tool[] newActiveTools = new Tool[activeTools.Length - 1];
+            int j = 0;
+            for (int i = 0; i < activeTools.Length; i++)
+            {
+                if (activeTools[i] != tool)
+                {
+                    newActiveTools[j] = activeTools[i];
+                    j++;
+                }
+                else
+                {
+                    activeTools[i].Deselect();
+                }
+            }
+            activeTools = newActiveTools;
+        }
+    }
+    private void DeselectTools()
+    {
+        if (activeTools != null)
+        {
+            foreach (Tool tool in activeTools)
+            {
+                tool.Deselect();
+            }
+            activeTools = null;
+        }
+    }
+
+    public void SetPlayers(GameObject[] players)
+    {
+        this.players = players;
+    }
 
 }
-
-// public abstract class Tool : MonoBehaviour
-// {
-//     private GameObject[] players;
-//     public abstract void Select();
-//     public abstract void Deselect();
-//     public abstract void Update();
-// }
-
-// public class CalculateDistance : Tool
-// {
-//     public override void Select()
-//     {
-//         Debug.Log("Distance tool selected");
-//     }
-
-//     public override void Deselect()
-//     {
-//         Debug.Log("Distance tool deselected");
-//     }
-
-//     public override void Update()
-//     {
-//         Debug.Log("Calculating distance");
-//     }
-// }
